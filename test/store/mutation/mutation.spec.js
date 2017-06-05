66c14888bdfca9acd7ee03935e3d3d717e2070eb
Django <jsjang@coupang.com>
2017-06-05 16:03:53 +0900

    [MEAN] 테스트 케이스 추가

--- test/store/mutation/mutation.spec.js
+++ test/store/mutation/mutation.spec.js
@@ -1,14 +1,22 @@
 import mutations from '../../../src/store/mutations';
 
-const { ADD_TODO } = mutations;
+const { ADD_TODO, REMOVE_TODO } = mutations;
 
-describe('TODO 변이 검사', () => {
+let state = {};
+state.todoList = [{ _id : 0, text : '테스트', complete : false }];
+
+describe('TODO 추가 mutation 검사', () => {
     it('ADD_TODO', () => {
-        let state = {};
-        state.todoList = [{ _id : 0, text : '테스트', complete : false }];
-
         ADD_TODO(state, { text : '테스트추가', _id : 1 });
 
         expect(state.todoList.length).toBe(2);
+    })
+});
+
+describe('TODO 삭제 mutation 검사', () => {
+    it('REMOVE_TODO', () => {
+        REMOVE_TODO(state, { _id : 0 });
+
+        expect(state.todoList.length).toBe(1);
     })
 });
