201a28d08a94f7148a9b870794a4e98a88543882
Django <jsjang@coupang.com>
2017-06-05 16:06:46 +0900

    [MEVN] 테스트 케이스 수정

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
-import Component from '../src/components/todoAdd.vue';
+import Component from '../src/app.vue';
 
-describe("할 일 추가 컴포넌트 검사", () => {
-    it('추가 함수가 동작하는지', () => {
-        expect(typeof Component.methods.add).toBe('function');
+describe("TODO 컨테이너 함수 검사", () => {
+    it('beforeMount', () => {
+        expect(typeof Component.beforeMount).toBe('function');
     });
 });
